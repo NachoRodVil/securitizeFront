0808fdd62f697e08fe1b91bfac854f1547e2befd
Ignacio Rodriguez Villasuso <ignaciorodvil@gmail.com>
2022-05-10 19:13:29 -0300

    Heroku Fixes

--- src/components/walletsHome/utils/utils.tsx
+++ src/components/walletsHome/utils/utils.tsx
@@ -1,20 +1,21 @@
 import axios from "axios";
 import { ExchangeRate, Wallet } from "./types";
 
+const API_URL = "https://securitize-challenge-api.herokuapp.com/"
 
 async function fetchWallets(): Promise<Wallet[]> {
-    const data = await axios.get("/wallets");
+    const data = await axios.get(`${API_URL}/wallets`);
     return data.data
 }
 
 async function fetchER(): Promise<ExchangeRate[]> {
-    const data = await axios.get("/exchangeRates");
+    const data = await axios.get(`${API_URL}/exchangeRates`);
     return data.data
 }
 
 async function updateER(id:string, value: number){
     try{
-    const response = await axios.patch(`/exchangeRates/${id}`,{value})
+    const response = await axios.patch(`${API_URL}/exchangeRates/${id}`,{value})
     return response
     } catch(error){
         return error
@@ -23,7 +24,7 @@ async function updateER(id:string, value: number){
 
 async function updateWalletEth(id:string, eth: number){
     try{
-    const response = await axios.patch(`/wallets/eth/${id}`,{eth})
+    const response = await axios.patch(`${API_URL}/wallets/eth/${id}`,{eth})
     return response
     } catch(error){
         return error
@@ -32,7 +33,7 @@ async function updateWalletEth(id:string, eth: number){
 
 async function updateWalletFav(id:string){
     try{
-    const response = await axios.patch(`/wallets/${id}`)
+    const response = await axios.patch(`${API_URL}/wallets/${id}`)
     return response
     } catch(error){
         return error
